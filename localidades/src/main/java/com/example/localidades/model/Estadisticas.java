78a342cc70e08f67cc922d1f98c779abb8a21e6d
MP-Alumno <MP-Alumno@duoc.acad>
2026-06-16 15:10:52 -0400

    Agregados algunas descripciones de swagger, faltan por hacer

--- localidades/src/main/java/com/example/localidades/model/Estadisticas.java
+++ localidades/src/main/java/com/example/localidades/model/Estadisticas.java
@@ -1,15 +1,28 @@
 package com.example.localidades.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
+
+@Schema(description = "Entidad que representa que cosas se considerarán para las estadísticas estadísticas")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Estadisticas {
+
+
     private Long totalLocalidades;
+
+    @Schema(description = "capacidad aproximada en las localidades registradas")
     private Long capacidadPromedio;
+
+    @Schema(description = "Mayor capacidad que se encuentra en las localidades")
     private Long mayorCapacidad;
+
+    @Schema(description = "Menor capacidad que se encuentra en las localidades")
     private Long menorCapacidad;
 }
